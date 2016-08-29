7df9e2bad775891b68c0a36eaa716499bd0f95bd
Vishal Banthia <vishal.banthia.vb@gmail.com>
2016-08-29 23:20:45 +0530

    Move tests to android

---
+++ e2e-tests/spec/features/multiplication_spec.rb
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+RSpec.describe 'Calculator' do
+  let!(:x) { 7 }
+  let!(:y) { 3 }
+
+  context 'multiplication' do
+    before do
+      $driver.find_element(:accessibility_id, 'resetButton').click
+
+      sleep 1
+      $driver.find_element(:accessibility_id, 'inputFieldLeft').send_key(x)
+      $driver.find_element(:accessibility_id, 'inputFieldRight').send_key(y)
+
+      sleep 1
+    end
+
+    after do |example|
+      if example.exception
+        example.metadata[:failed_screenshot] = Screenshot.capture_screenshot
+      end
+    end
+
+    it 'multiply two numbers' do
+      $driver.find_element(:accessibility_id, 'multiplicationButton').click
+      sleep 1
+
+      result_text = $driver.find_element(:accessibility_id, 'resultTextView').text
+      expected_result = "%.2f" % x + " * " + "%.2f" % y + " = " + "%.2f" % (x * y)
+      expect(result_text).to eq(expected_result)
+    end
+  end
+end
---
+++ e2e-tests/spec/spec_helper.rb
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+require 'appium_lib'
+
+# support files
+SPEC_ROOT = File.expand_path(File.dirname(__FILE__))
+Dir[File.expand_path('support/**/*.rb', SPEC_ROOT)].each { |f| require f }
+
+app_path = ENV['APK_PATH'] || raise('please specify apk path by setting ENV["APK_PATH"]')
+udid = ENV['UDID'] || raise('please specify device serial by setting ENV["UDID"]')
+
+RSpec.configure do |config|
+
+  config.include ::Screenshot
+
+  config.before(:suite) do
+    driver_caps = {
+      platformName: :android,
+      deviceName: 'Android',
+      newCommandTimeout: 9999,
+      app: app_path,
+      udid: udid
+    }
+
+    Appium::Driver.new(caps: driver_caps).start_driver
+  end
+
+  config.after(:suite) do
+    $driver.driver_quit
+  end
+end
